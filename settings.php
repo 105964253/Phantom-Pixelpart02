cf98b0da0bc1b0cef92524531e833c9df0b13b2a
105964253 <105964253@student.swin.edu.au>
2025-10-17 11:55:11 +1100

    Added in the code for the connection to database function and code to check if connection is a success between the settings.php and database.

--- settings.php
+++ settings.php
@@ -3,6 +3,13 @@
 $host = "localhost";
 $user = "root";
 $pwd = " ";
-$sql_db = " "; // add in data base later on
+$sql_db = " "; // add in database later on
 
+// Connection function
+$conn = mysqli_connect("localhost", "root", " ", " database name")
+
+// Check the connection to see if it is functioning correctly
+if (!conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
