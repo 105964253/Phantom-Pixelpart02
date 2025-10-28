5b604b2e4029789c0de92ed8aa345cdb888286a3
105964253 <105964253@student.swin.edu.au>
2025-10-28 16:09:34 +1100

    Fixed password requirement in settings.php. Taking away the spacing so that it indicates that no password is needed.

--- settings.php
+++ settings.php
@@ -6,7 +6,7 @@ $pwd = "";
 $sql_db = "phantom_pixel"; // add in database later on
 
 // Connection function
-$conn = mysqli_connect("localhost", "root", " ", "phantom_pixel");
+$conn = mysqli_connect("localhost", "root", "", "phantom_pixel");
 
 // Check the connection to see if connection is uncessful
 if (!$conn) {
